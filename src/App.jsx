88a2b867b0a10974f34b025826f1528bcc1a5141
Ani no <bryanlomerioanino@gmail.com>
2024-09-03 10:16:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Qr from "./components/Qr.jsx"
+
+
+function App() {
+  return (
+    <>
+  <Qr/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Qr.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import QRCode from "react-qr-code"
+import "./Qr.css"
+
+function Qr(){
+
+    const [qrCode, setQrCode] = useState('')
+    const [input, setInput] = useState('')
+    function handleGenerateQrCode(){
+        setQrCode(input)
+    }
+
+
+return(
+<>
+<div className="input-btn">
+    <input onChange={(e) => setInput(e.target.value)} type="text" name="generate" placeholder="Enter you value" />
+<button disabled={input && input.trim() !== "" ? false : true} onClick={handleGenerateQrCode}>Generate Qr</button>
+</div>
+<div>
+    <QRCode
+    className="qr"
+    id="qr-code-value" value={qrCode}
+    />
+</div>
+
+</>
+);
+}
+export default Qr;
